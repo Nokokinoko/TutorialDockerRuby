ee05ce957aca6a0662161ab266fbc82859270c0a
津田 翔大 <syota.t@gmail.com>
2019-05-08 20:55:40 +0900

    add: relationship

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# user
 User.create!(
   name: "Example User",
   email: "example@railstutorial.org",
@@ -28,8 +22,17 @@ User.create!(
   )
 end
 
+# micropost
 users = User.order(:created_at).take(6)
 10.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# relationship
+users = User.all
+user = users.first
+following = users[2..50]
+following.each { |followed| user.follow(followed) }
+followers = users[3..40]
+followers.each { |follower| follower.follow(user) }

800df91306213b9068d1c6f7188c9c99b89e5bbf
津田 翔大 <syota.t@gmail.com>
2019-06-12 03:46:46 +0900

    resume from ch.5

--- src/test/controllers/static_pages_controller_test.rb
+++ src/test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,17 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get root_path
+    get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
-    get help_path
+    get static_pages_help_url
     assert_response :success
   end
 
   test "should get about" do
-    get about_path
-    assert_response :success
-  end
-  
-  test "should get contact" do
-    get contact_path
+    get static_pages_about_url
     assert_response :success
   end
 end

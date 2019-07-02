7c99af47b44dcd894bb7e689dbbb3221d6dbc442
津田 翔大 <syota.t@gmail.com>
2019-07-02 22:34:34 +0900

    add: ch.11

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  
-  # emailを直接変更
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -30,13 +29,36 @@ class User < ApplicationRecord
   end
   
   # トークンとダイジェストを比較
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
   
   # ログイン情報を破棄
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  # アカウント有効化
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+  
+  # 有効化メールを送信
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+  
+  private
+    # メールアドレスを小文字に変換
+    def downcase_email
+      self.email.downcase!
+    end
+    
+    # 有効化トークンとダイジェストを作成
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
--- src/test/integration/users_signup_test.rb
+++ src/test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invalid signup information" do
     get signup_path
     # ユーザ登録(失敗)後にユーザ数が変わらないことをテスト
@@ -13,9 +17,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
   
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     # ユーザ登録(成功)後にユーザ数が+1していることをテスト
     assert_difference 'User.count', 1 do
@@ -26,6 +32,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         password_confirmation: "password"
       } }
     end
+    # 配信メッセージが1件であることをテスト
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    # Userコントローラの@userにアクセス
+    user = assigns(:user)
+    # 有効化が完了していないことをテスト
+    assert_not user.activated?
+    log_in_as(user)
+    # ログイン出来ないことをテスト
+    assert_not is_logged_in?
+    # 有効化トークンが不正
+    get edit_account_activation_path("Invalid token", email: user.email)
+    assert_not is_logged_in?
+    # メールアドレスが不正
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 有効化
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?

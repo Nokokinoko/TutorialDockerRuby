68ba30a94d7748fa7829ebd14905d4a9ea1072e4
津田 翔大 <syota.t@gmail.com>
2019-10-21 01:25:19 +0900

    add: ch.6

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_191435) do
+ActiveRecord::Schema.define(version: 2019_10_20_161528) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
